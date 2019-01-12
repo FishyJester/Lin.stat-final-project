eca6f8331e1f6ca99b8199e1f5d6bfc11a2e416a
FishyJester <vetekorn@gmail.com>
2019-01-12 15:58:35 +0100

    Add files via upload
    Added more things, US state map, dummy coding, smaller things

--- final.R
+++ final.R
@@ -16,7 +16,7 @@ data = CDIdata
 data[,1]= 1000*(CDIdata$crimes/CDIdata$popul)
 colnames(data)[1]="crmpp"
 
-# Set individual names on county's and assign them as row.names()
+# Set individual names on county's and assign them as row.names()----------------------------------------------
 n_occur <- data.frame(table(data$county))
 tmp = data[data$county %in% n_occur$Var1[n_occur$Freq > 1],c(2, 3)]
 tmp = within(tmp, new <- paste(county, state, sep = "_"))
@@ -32,7 +32,7 @@ for (i in 1:48) {
   data$state[data$state == states[i]] = i
 }
 
-# correlation matrix,to detect possible colinearities
+# correlation matrix,to detect possible colinearities-------------------------------------------------------
 library(RColorBrewer)
 library(ggplot2)
 library(reshape2)
@@ -62,7 +62,7 @@ ggplot(data = corrMat, aes(Var1, Var2)) + geom_tile(aes(fill = value)) +
                                title.position = "top", title.hjust = 0.5))
 
 # Clearly strong correlation at totalincome and crime
-# Removing totalincome and crime (see appendix for argumentation)
+# Removing totalincome and crime (see appendix for argumentation)-------------------------------------------
 drops = c("totalincome", "crimes")
 data = data[, !(names(data) %in% drops)]
 rm(drops)
@@ -72,7 +72,7 @@ data$phys = 1000*(data$phys/data$popul)
 data$beds = 1000*(data$beds/data$popul)
 names(data)[names(data) %in% c("phys", "beds")] = c("physpp", "bedspp")
 
-#Redo the correlation matrix to check
+#Redo the correlation matrix to check---------------------------------------------------------------------
 corrMat = round(cor(data[-c(2, 14)]), 2) #Remove states and region
 corrMat[upper.tri(corrMat)] = NA
 corrMat = data.frame(melt(corrMat, na.rm = TRUE))
@@ -96,7 +96,8 @@ ggplot(data = corrMat, aes(Var1, Var2)) + geom_tile(aes(fill = value)) +
   guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
                                title.position = "top", title.hjust = 0.5))
 
-# Looks good now, continuing onwards let's check independent vs dependent
+# Looks good now, continuing onwards let's check independent vs dependent------------------------------------
+library(tidyr)
 data2 = gather(data, dat, value, -c(crmpp, state, region))
 ggplot(data = data2, aes(value, crmpp, color = crmpp)) + geom_point() +
   theme(legend.position = "none") +
@@ -104,32 +105,101 @@ ggplot(data = data2, aes(value, crmpp, color = crmpp)) + geom_point() +
   theme(axis.text.x= element_text(size = 6, angle= 45))
 
 
-# Need some major transforms. Log on: area, popul, maybe physpp? ALso, one MAJOR outlier, what is this?
+# Need some major transforms. Log on: area, popul, maybe physpp? ALso, one MAJOR outlier, what is this?-------
 data.transf = data
 data.transf$area = log(data.transf$area)
 data.transf$popul = log(data.transf$popul)
 data.transf$physpp = log(data.transf$physpp)
 
-names(data.transf)[names(data.transf) %in% c("area", "popul", "physpp")] = c("log(area)", "log(popul)", "log(physpp)")
+names(data.transf)[names(data.transf) %in% 
+                     c("area", "popul", "physpp")] = 
+  c("log(area)", "log(popul)", "log(physpp)")
 
 #Redo plots
+library(tidyr)
 data2 = gather(data.transf, dat, value, -c(crmpp, state, region))
 ggplot(data = data2, aes(value, crmpp, color = crmpp)) + geom_point() +
   theme(legend.position = "none") +
   facet_wrap(~dat, scales = "free") +
-  theme(axis.text.x= element_text(size = 6, angle= 45))
+  theme(axis.text.x= element_text(size = 6, angle= 45))+
+  geom_text(aes(label = ifelse(crmpp > 200, as.character(state), '')), hjust = -0.5, vjust = 0.5, size = 2)
 
-# OK! Now let's check crime levels in the different states
+# OK! Now let's check crime levels in the different states----------------------------------------
 
 ggplot(data, aes(x=factor(region), y=crmpp, group=state))+
   geom_boxplot(fill="red", alpha=0.2, outlier.colour = "black", outlier.alpha = 1)+
   geom_text(aes(label=ifelse(crmpp>150,as.character(state),'')),hjust=0,vjust=-0.5)+
   scale_x_discrete(name = "Regions", labels = c("Northeast", "Midwest", "South", "West"))
 
-# Let's plot crmpp over map of US
+# Let's plot crmpp over map of US-------------------------------------------------------------------------------------
 library(maps)
+library(ggplot2)
 state.map = map_data(map = "state")
 
+#Need to rename all states in data to full state name as in map_data. Annoying...
+n.obs = dim(data)[1]
+names.col = rep(NA, n.obs)
+
+# Check which states are missing from our data, remove these 
+# from abb.
+states.abb = state.abb[!is.na(match(state.abb, data$state))]
+states.names = state.name[!is.na(match(state.abb, data$state))]
+
+# This is for DC, not existing in state.name so we insert it manually
+names.col[match("DC", as.character(data$state))] = "district of columbia" 
+
+for(i in 1:nlevels(data$state))
+  names.col[grep(states.abb[i], data$state)] = tolower(states.names[i])
+
+imp.map.vars = data.frame("crm" = data$crmpp, "region" = names.col, "stateAbb" = as.character(data$state))
+
+# Now we just join the crm data with the appropriate state in the map_data
+library(dplyr)
+state.map = left_join(state.map, imp.map.vars, by = "region")
+tmp = state.map %>% group_by(region) %>% summarise(avgcrm = mean(crm), n = n())
+state.map = left_join(state.map, tmp, by = "region")
+
+rm(imp.map.vars, tmp)
+
+# And now we can finally plot
+library(viridis)
+library(ggthemes)
+no.data = unique(state.map$region[which(is.na(state.map$crm))])
+
+ggplot(data = state.map, mapping = aes(x = long, y = lat, group = group))+
+  geom_polygon(color = "white", aes(fill = avgcrm)) +
+  scale_fill_viridis(option="viridis", na.value = "white")+
+  theme_fivethirtyeight()+
+  theme(
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    legend.text = element_text(size = 9),
+    legend.title = element_blank()
+  )+
+  annotate("text", x = -93.7, y = 41.955, label = "No data", size = 2)+
+  annotate("text", x = -108.18, y = 42.8, label = "No data", size = 2)+
+  coord_map(projection = "conic", lat0 = 30)+
+  labs(title = "Average crime rate per 1000 inhabitants per state.",
+       subtitle = "Data from 1990 census.", size = 0.5)
+  
+#--------------------------------------------------------------------------------------------------------------------
+#
+# Plotting done. To efficiently work with model selection (regsubset) later we need to create dummy variables
+# out of the region variable. Question is; should we have state as independent variable? Pros, cons?
+# Let's leave it out for now. Too many variables if we would treat it as a factor.
+data.transf = data.transf[,-2] # Removing state variable
+data.transf$region = as.factor(data.transf$region)
+# Need to create dummy variables out of region with "West" as baseline.
+library(dummies)
+data.transf = dummy.data.frame(data.transf)
+data.transf = data.transf[,-16] # Remove region 4 (West) to use as baseline.
+names(data.transf)[names(data.transf) %in% 
+                     c("region1", "region2", "region3")] = 
+  c("Northeast", "Midwest", "South")
 
 # hist(data$crmpp)
 hist(log(data$crmpp))
